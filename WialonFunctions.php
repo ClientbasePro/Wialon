6234c50cc1afc7c58943d1f51c8d13a101e0c13f
ClientbasePro <42553531+ClientbasePro@users.noreply.github.com>
2020-01-28 10:46:34 +0300

    Обновление
    Добавил параметр $URL в функции для рбаоты с Local версиями Wialon

--- WialonFunctions.php
+++ WialonFunctions.php
@@ -4,10 +4,11 @@
   // https://ClientbasePro.ru
   // docs.wialon.com, https://sdk.wialon.com/wiki/ru/sidebar/start
 
-  // функция выполняет произвольный запрос к Wialon к сервису (string)$someService с параметрами (array)$params и (array)$toGETparams
-function GetWialonData($someService='', $params=[], $toGETparams=[]) {
+  // функция выполняет произвольный запрос к Wialon к сервису (string)$someService с параметрами (array)$params и (array)$toGETparams на URL $someURL
+function GetWialonData($someService='', $params=[], $toGETparams=[], $someURL='') {
   if (!$someService) return false;
-  $url = WIALON_URL;
+  $url = ($someURL) ? $someURL : (defined(WIALON_URL)?WIALON_URL:'');
+  if (!$url) return false;
   $url .= $someService;
   $url .= ($params) ? '&params='.json_encode($params) : '&params={}';
   if ($toGETparams) $url .= '&'.http_build_query($toGETparams);  
@@ -17,18 +18,18 @@ function GetWialonData($someService='', $params=[], $toGETparams=[]) {
 }
 
   // функция возвращает Wialon Session ID
-function GetWialonSID($token) {
+function GetWialonSID($token, $someURL='') {
   if (!$token && defined(WIALON_TOKEN)) $token = WIALON_TOKEN;
   if (!$token) return false;
-  $answer = json_decode(GetWialonData('token/login',array('token'=>$token)), true); 
-  if ($answer=$answer['eid']) return $answer;
+  $answer = json_decode(GetWialonData('token/login',array('token'=>$token), 0, $someURL), true); 
+  if ($answer_=$answer['eid']) return $answer_;
   return false;
 }
 
   // функция разлогинивает подключение к WIALON, возвращает bool результат
-function LogoutWialon($SID='') {
+function LogoutWialon($SID='', $someURL='') {
   if (!$SID) return false;
-  $answer = json_decode(GetWialonData('core/logout', '', array('sid'=>$SID)), true);
+  $answer = json_decode(GetWialonData('core/logout', 0, array('sid'=>$SID), $someURL), true);
   if (0==$answer['error']) return true;
   return false;
 }
